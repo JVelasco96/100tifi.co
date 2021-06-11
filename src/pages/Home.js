d9f133a54c9c3fbb8e22b4827c5dac3a2e381148
Javier Velasco <xavier_anotonio@hotmail.com>
2021-06-10 18:40:12 -0600

    Pages & tamplates creados, iniciando con las rutas

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    const view = `
+        <div class="character">
+            <article class="character-item">
+                <a href="#/1/">
+                    <img src="" alt=""/>
+                    <h2></h2>
+                </a>
+            </article>
+        </div>    
+    `;
+
+    return view;
+}
+
+export default Home;
