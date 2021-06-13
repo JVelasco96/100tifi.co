92900b37688eb1e20208b2cf14e50c7439e4005d
Javier Velasco <xavier_anotonio@hotmail.com>
2021-06-13 14:34:46 -0600

    Creacion de rutas terminadas, empezamos con la consulta de la API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 const Home = () => {
     const view = `
-        <div class="character">
-            <article class="character-item">
-                <a href="#/1/">
+        <div class="Character">
+            <article class="Character-item">
+                <a href="/#/1/">
                     <img src="" alt=""/>
-                    <h2></h2>
+                    <h2>Name</h2>
                 </a>
             </article>
         </div>    
